89611db65083a87756771b4f072a23a5f3f3d71f
akshatbhandari11 <akshatbhandari11@gmail.com>
2023-11-27 15:02:55 +0530

    finally flak worked

---
+++ src/Malignant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Malignant = () => {
+  return (
+    <div>
+      <h2>Malignant Component</h2>
+      <p>This is a test component for Malignant cases.</p>
+    </div>
+  );
+};
+
+export default Malignant;
